3b5b94fb4545fa63f78d8d14d5d98ef12697ac7c
banana <ufozhengli@163.com>
2015-09-24 16:54:49 +0800

    init lean_messenger

---
+++ lib/lean_messenger.rb
@@ -0,0 +1,10 @@
+require "lean_messenger/version"
+
+module LeanMessenger
+  # Your code goes here...
+  class Messenger
+    def self.hi
+      puts "Hello world!"
+    end
+  end
+end
---
+++ lib/lean_messenger/version.rb
@@ -0,0 +1,3 @@
+module LeanMessenger
+  VERSION = "0.1.0"
+end
