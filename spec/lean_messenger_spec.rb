7c208e98fe7b70f76c9a18087aa0f44605a3d101
banana <ufozhengli@163.com>
2015-09-24 19:21:16 +0800

    add rspec support

---
+++ spec/lean_messenger_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'awesome_print'
+
+describe LeanMessenger do
+  subject { LeanMessenger::Messenger.new }
+  it "" do
+    
+  end
+  
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'lean_messenger'
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+
+end
